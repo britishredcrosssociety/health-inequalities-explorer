8d0056133de508b59c0c78a7043fba65434759d1
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2022-05-17 18:09:04 +0200

    Update name

--- data-raw/lad-england.R
+++ data-raw/lad-england.R
@@ -3,9 +3,9 @@ library(sf)
 library(geographr)
 library(rmapshaper)
 
-boundaries_ltla21_enlgand <-
+boundaries_ltla21_england <-
   boundaries_ltla21 |>
   filter(str_detect(ltla21_code, "^E")) |>
   ms_simplify(keep = 0.5) # Any less and areas get dropped
 
-usethis::use_data(boundaries_ltla21_enlgand, overwrite = TRUE)
+usethis::use_data(boundaries_ltla21_england, overwrite = TRUE)
