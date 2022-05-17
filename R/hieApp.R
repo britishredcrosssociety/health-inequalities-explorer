ad796a74451c88b436fb27d013841d3a87f54b47
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2022-05-17 17:27:35 +0200

    Add logo & instructions

--- R/hieApp.R
+++ R/hieApp.R
@@ -1,10 +1,45 @@
 library(shiny)
 
 hieApp <- function() {
+
+  # ---- UI ----
   ui <- fluidPage(
 
-  )
+    # - Set CSS -
+    includeCSS("inst/www/styles.css"),
 
+    # - Logo -
+    fluidRow(
+      align = "center",
+      tags$div(
+        class = "brc-logo",
+        tags$a(
+          href = "https://redcross.org.uk",
+          target = "_blank",
+          img(src = "www/brc-team-logo.jpg", width = 400)
+        ) # tags$a
+      ) # tags$div
+    ), # fluidRow
+
+    # - Instructions -
+    fluidRow(
+      column(width = 2),
+      column(
+        width = 8,
+        align = "center",
+        tags$h1(
+          "Health Inequalities Explorer"
+        ), # tags$h1
+        tags$p(
+          "You can use this tool to explore health inequalities and how they
+          compare across different geographies across the UK."
+        ) # tags$p
+      ), # column
+      column(width = 2)
+    ) # fluidRow
+  ) # fluidPage
+
+  # ---- Server ----
   server <- function(input, output, session) {
 
   }
