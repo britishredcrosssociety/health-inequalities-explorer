0ef02d6369af7f411d60e5d7a0ae666eb61aecc5
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2022-05-19 16:57:36 +0200

    Update names

--- data-raw/lad-england.R
+++ data-raw/lad-england.R
@@ -6,6 +6,7 @@ library(rmapshaper)
 boundaries_ltla21_england <-
   boundaries_ltla21 |>
   filter(str_detect(ltla21_code, "^E")) |>
+  mutate(ltla21_name = str_replace_all(ltla21_name, "'", "")) |>
   ms_simplify(keep = 0.5) # Any less and areas get dropped
 
 usethis::use_data(boundaries_ltla21_england, overwrite = TRUE)
