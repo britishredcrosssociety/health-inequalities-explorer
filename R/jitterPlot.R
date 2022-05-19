6a6de8b1c315d8b6c560c94ab5521f74481b4dbd
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2022-05-19 16:58:20 +0200

    Add demo

--- R/jitterPlot.R
+++ R/jitterPlot.R
@@ -6,8 +6,25 @@ jitterPlotUI <- function(id) {
 
 jitterPlotServer <- function(id) {
   moduleServer(id, function(input, output, session) {
+    gg <- hi_vul_england |>
+      ggplot(aes(x = value, y = name)) +
+      geom_vline(xintercept = 150, size = 2, alpha = .5, colour = "#FFA500") +
+      geom_jitter_interactive(
+        aes(tooltip = ltla21_name, data_id = ltla21_name),
+        height = 0.25, color = "steelblue3", size = 4, alpha = .7
+      ) +
+      theme_minimal() +
+      labs(x = NULL, y = NULL)
+
     output$plot <- renderGirafe({
-      girafe(ggobj = gg)
+      girafe(
+        ggobj = gg,
+        options = list(
+          opts_hover_inv(css = "opacity:0.3;"),
+          opts_toolbar(saveaspng = FALSE),
+          opts_selection(type = "none")
+        )
+      )
     })
   })
 }
