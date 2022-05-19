3d98b4883f1a28c555b0cb96797afc8fafcd7ef7
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2022-05-19 16:58:14 +0200

    Remove commas from names

--- data-raw/health-index-england.R
+++ data-raw/health-index-england.R
@@ -3,8 +3,8 @@ library(geographr)
 library(sf)
 
 ltla_names <-
-  boundaries_ltla21 |> 
-  st_drop_geometry() |> 
+  boundaries_ltla21 |>
+  st_drop_geometry() |>
   select(starts_with("ltla21_"))
 
 raw <- read_csv(
@@ -13,40 +13,15 @@ raw <- read_csv(
 
 hi_vul_england <-
   raw |>
-  left_join(ltla_names, by = c("lad_21_code" = "ltla21_code")) |> 
+  left_join(ltla_names, by = c("lad_21_code" = "ltla21_code")) |>
+  mutate(ltla21_name = str_replace_all(ltla21_name, "'", "")) |> 
   select(
     ltla21_name,
     `Overall Score` = overall_score_rank,
     `Healthy People` = healthy_people_rank,
     `Healthy Lives` = healthy_lives_rank,
     `Healthy Places` = healthy_places_rank
-  ) |> 
+  ) |>
   pivot_longer(cols = -ltla21_name)
 
-# usethis::use_data(hi_vul_england, overwrite = TRUE)
-
-hi_vul_england |> 
-  ggplot(aes(x = value, y = name)) +
-  geom_vline(xintercept = 150, size = 2, alpha = .5, colour = "#FFA500") +
-  geom_jitter(height = 0.25, color = "steelblue3", size = 5, alpha = .5) +
-  theme_minimal() +
-  labs(x = NULL, y = NULL)
-
-library(ggiraph)
-
-gg <- hi_vul_england |>
-mutate(ltla21_name = str_replace_all(ltla21_name, "'", "")) |> 
-  ggplot(aes(x = value, y = name)) +
-  geom_vline(xintercept = 150, size = 2, alpha = .5, colour = "#FFA500") +
-  geom_jitter_interactive(
-    aes(tooltip = ltla21_name, data_id = ltla21_name),
-    height = 0.25, color = "steelblue3", size = 5, alpha = .5
-    ) +
-  theme_minimal() +
-  labs(x = NULL, y = NULL)
-
-girafe(code = print(gg))
-
-# alter non hovered elemts
-# https://davidgohel.github.io/ggiraph/articles/offcran/customizing.html
-
+usethis::use_data(hi_vul_england, overwrite = TRUE)
