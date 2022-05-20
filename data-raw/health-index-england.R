fecf224dc44af2691d0831c38641ec775e29ce66
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2022-05-20 12:07:58 +0200

    Update data sets to use consistent colnames

--- data-raw/health-index-england.R
+++ data-raw/health-index-england.R
@@ -14,14 +14,14 @@ raw <- read_csv(
 hi_vul_england <-
   raw |>
   left_join(ltla_names, by = c("lad_21_code" = "ltla21_code")) |>
-  mutate(ltla21_name = str_replace_all(ltla21_name, "'", "")) |> 
+  mutate(ltla21_name = str_replace_all(ltla21_name, "'", "")) |>
   select(
-    ltla21_name,
+    area_name = ltla21_name,
     `Overall Score` = overall_score_rank,
     `Healthy People` = healthy_people_rank,
     `Healthy Lives` = healthy_lives_rank,
     `Healthy Places` = healthy_places_rank
   ) |>
-  pivot_longer(cols = -ltla21_name)
+  pivot_longer(cols = -area_name, names_to = "variable")
 
 usethis::use_data(hi_vul_england, overwrite = TRUE)
--- data-raw/lad-england.R
+++ data-raw/lad-england.R
@@ -7,6 +7,7 @@ boundaries_ltla21_england <-
   boundaries_ltla21 |>
   filter(str_detect(ltla21_code, "^E")) |>
   mutate(ltla21_name = str_replace_all(ltla21_name, "'", "")) |>
-  ms_simplify(keep = 0.5) # Any less and areas get dropped
+  ms_simplify(keep = 0.5) |> # Any less and areas get dropped
+  select(area_name = ltla21_name)
 
 usethis::use_data(boundaries_ltla21_england, overwrite = TRUE)
