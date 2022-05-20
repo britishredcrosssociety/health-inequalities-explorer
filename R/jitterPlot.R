e036ab039467444e0aca2330a81890b3fe3f8e11
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2022-05-20 18:09:24 +0200

    Color plot by selected area

--- R/jitterPlot.R
+++ R/jitterPlot.R
@@ -4,19 +4,56 @@ jitterPlotUI <- function(id) {
   )
 }
 
-jitterPlotServer <- function(id, data) {
+jitterPlotServer <- function(id, data, selected_area) {
   moduleServer(id, function(input, output, session) {
-    gg <- data |>
-      ggplot(aes(x = value, y = variable)) +
-      geom_vline(xintercept = 150, size = 2, alpha = .5, colour = "#FFA500") +
-      geom_jitter_interactive(
-        aes(tooltip = area_name, data_id = area_name),
-        height = 0.25, color = "steelblue3", size = 4, alpha = .7
-      ) +
-      theme_minimal() +
-      labs(x = NULL, y = NULL)
-
     output$plot <- renderGirafe({
+
+      # Set sensible plot/legend default if no area has been selected
+      if (is.null(selected_area())) {
+        data <- data |>
+          dplyr::mutate(selected = "not selected")
+        legend_break_name <- NULL
+      } else {
+        data <- data |>
+          dplyr::mutate(
+            selected = dplyr::if_else(
+              area_name == selected_area(),
+              area_name,
+              "not selected"
+            )
+          ) |>
+          dplyr::mutate(
+            selected = factor(
+              selected,
+              levels = c("not selected", selected_area())
+            )
+          )
+        legend_break_name <- selected_area()
+      }
+
+      # Create plot object
+      gg <- data |>
+        ggplot(aes(x = value, y = variable, colour = selected)) +
+        geom_vline(
+          xintercept = 150, size = 2, alpha = .5, colour = "#FFA500"
+        ) +
+        geom_jitter_interactive(
+          aes(tooltip = area_name, data_id = area_name),
+          height = 0.25,
+          size = 3, alpha = .7
+        ) +
+        theme_minimal() +
+        theme(
+          legend.position = "top",
+          legend.title = element_blank()
+        ) +
+        scale_color_viridis_d(
+          option = "C", alpha = .5, begin = .2, end = .8,
+          breaks = legend_break_name
+        ) +
+        labs(x = NULL, y = NULL)
+
+      # Render plot
       girafe(
         ggobj = gg,
         options = list(
@@ -36,10 +73,11 @@ jitterPlotTest <- function(data) {
   )
 
   server <- function(input, output, session) {
-    jitterPlotServer("test", data)
+    selected_area <- reactiveVal()
+    jitterPlotServer("test", data, selected_area)
   }
 
-  shinyApp(ui, server) 
+  shinyApp(ui, server)
 }
 
 # Examples
