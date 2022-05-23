6261c946ad677335274c758bcb83b94e858e2333
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2022-05-23 14:54:07 +0200

    Allow selection of multiple areas

--- R/map.R
+++ R/map.R
@@ -5,13 +5,10 @@ mapUI <- function(id) {
   )
 }
 
-mapServer <- function(id, selected_area, data) {
+mapServer <- function(id, data, selected) {
   moduleServer(id, function(input, output, session) {
-    observeEvent(input$map_shape_click, {
-      input$map_shape_click$id |>
-        selected_area()
-    })
 
+    # Create static elemets
     output$map <-
       renderLeaflet({
         leaflet() |>
@@ -20,21 +17,53 @@ mapServer <- function(id, selected_area, data) {
           addPolygons(
             data = data,
             layerId = ~area_name,
+            group = "base",
+            label = ~area_name,
             weight = 0.7,
             opacity = 0.5,
             color = "#5C747A",
             dashArray = "0.1",
+            fillOpacity = 0.2,
+            highlight = highlightOptions(
+              weight = 5,
+              color = "#5C747A",
+              dashArray = "",
+              fillOpacity = 0.2,
+              bringToFront = TRUE
+            )
+          ) |>
+          addPolygons(
+            data = data,
+            layerId = ~area_code,
+            # Create number of groups equal to the length of the number of areas
+            # and match to click event ids from base layer
+            group = ~area_name,
+            label = ~area_name,
+            weight = 0.7,
+            opacity = 0.5,
+            color = "#D0021B",
+            dashArray = "0.1",
             fillOpacity = 0.4,
             highlight = highlightOptions(
               weight = 5,
-              color = "#666",
+              color = "#D0021B",
               dashArray = "",
-              fillOpacity = 0.7,
+              fillOpacity = 0.4,
               bringToFront = TRUE
-            ),
-            label = data$area_name
-          )
+            )
+          ) |>
+          hideGroup(data$area_name)
       })
+
+    observeEvent(input$map_shape_click, {
+      if (input$map_shape_click$group == "base") {
+        selected$areas <- c(selected$areas, input$map_shape_click$id)
+        leafletProxy("map") |> showGroup(input$map_shape_click$id)
+      } else {
+        selected$areas <- setdiff(selected$areas, input$map_shape_click$group)
+        leafletProxy("map") |> hideGroup(input$map_shape_click$group)
+      }
+    })
   })
 }
 
@@ -42,12 +71,13 @@ mapTest <- function(data) {
   ui <- fluidPage(
     mapUI("test")
   )
-
   server <- function(input, output, session) {
-    selected_area <- reactiveVal()
-    mapServer("test", selected_area, data)
+    selected <- reactiveValues(areas = NULL)
+    mapServer("test", data, selected)
+    observe({
+      print(selected$areas)
+    })
   }
-
   shinyApp(ui, server)
 }
 
