c5679dd9f3affec5748d24c5dc229bd635753f38
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2022-05-24 10:43:54 +0200

    Track changes in local and global reactive values and compare

--- R/map.R
+++ R/map.R
@@ -7,8 +7,6 @@ mapUI <- function(id) {
 
 mapServer <- function(id, data, selected) {
   moduleServer(id, function(input, output, session) {
-
-    # Create static elemets
     output$map <-
       renderLeaflet({
         leaflet() |>
@@ -36,7 +34,8 @@ mapServer <- function(id, data, selected) {
             data = data,
             layerId = ~area_code,
             # Create number of groups equal to the length of the number of areas
-            # and match to click event ids from base layer
+            # Match group names to layerId above so that group visibility can
+            # be toggled on/off
             group = ~area_name,
             label = ~area_name,
             weight = 0.7,
@@ -55,15 +54,54 @@ mapServer <- function(id, data, selected) {
           hideGroup(data$area_name)
       })
 
+    # Logic: create two sets of reactive values. One contained to the module
+    # namespace (`clicked`), and one to the global namespace (`selected`)
+
+    # Contained to local namespace
+    clicked <- reactiveValues(areas = vector())
+
+    # As polygons are clicked, update both the local and global reactive values
     observeEvent(input$map_shape_click, {
       if (input$map_shape_click$group == "base") {
         selected$areas <- c(selected$areas, input$map_shape_click$id)
+        clicked$areas <- c(clicked$areas, input$map_shape_click$id)
         leafletProxy("map") |> showGroup(input$map_shape_click$id)
       } else {
         selected$areas <- setdiff(selected$areas, input$map_shape_click$group)
+        clicked$areas <- setdiff(clicked$areas, input$map_shape_click$group)
         leafletProxy("map") |> hideGroup(input$map_shape_click$group)
       }
     })
+
+    # Track differences in the local and global reactive values. If activity has
+    # occured outside of this module, update the local reactive values to match
+    # the global reactive values and update the map polygons
+
+    observeEvent(selected$areas, {
+      removed <- setdiff(clicked$areas, selected$areas)
+      added <- setdiff(selected$areas, clicked$areas)
+
+      leafletProxy("map") |>
+        hideGroup(removed) |>
+        showGroup(added)
+
+      clicked$areas <- selected$areas
+    })
+
+    # observeEvent(selected$areas, {
+    #   removed <- setdiff(clicked$areas, selected$areas)
+    #   added <- setdiff(selected$areas, clicked$areas)
+
+    #   if (length(removed) > 0) {
+    #     clicked$areas <- selected$areas
+    #     leafletProxy("map") |> hideGroup(group = removed)
+    #   }
+
+    #   if (length(added) > 0) {
+    #     clicked$areas <- selected$areas
+    #     leafletProxy("map") |> showGroup(group = added)
+    #   }
+    # })
   })
 }
 
@@ -72,7 +110,7 @@ mapTest <- function(data) {
     mapUI("test")
   )
   server <- function(input, output, session) {
-    selected <- reactiveValues(areas = NULL)
+    selected <- reactiveValues(areas = vector())
     mapServer("test", data, selected)
   }
   shinyApp(ui, server)
