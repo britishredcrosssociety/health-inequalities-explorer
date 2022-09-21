a22ba0cb7c48a22447f6dabf53e097401f1b32ed
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2022-09-21 16:27:06 +0200

    remove

--- R/appServer.R
+++ R/appServer.R
@@ -2,7 +2,6 @@ server <- function(input, output, session) {
 
   # Load data sets (to be replaced withy dynamic selection from selected$geography)
   vulnerability <- ltla_vul_england
-  capacity <- ltla_cap_england
 
   # - Set an empty global reactive values list to be passed between modules -
   selected <- reactiveValues(areas = vector(), geography = vector())
