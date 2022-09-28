ecb49d4f78f09bb8f610467761ab2ad82ca58f18
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2022-09-28 11:29:08 +0200

    Scale distributions to +1/-1

--- data-raw/ltla-summary-metrics-england.R
+++ data-raw/ltla-summary-metrics-england.R
@@ -4,6 +4,7 @@ library(compositr)
 library(sf)
 library(IMD)
 library(readxl)
+library(ggridges)
 
 ltla <-
   boundaries_ltla21 |>
@@ -77,38 +78,42 @@ hackney_score <-
 
 health_index_missing_added <-
   health_index_2019 |>
-  add_row(ltla21_code = "E06000053", health_index_score = cornwall_score) |> 
+  add_row(ltla21_code = "E06000053", health_index_score = cornwall_score) |>
   add_row(ltla21_code = "E09000001", health_index_score = hackney_score)
 
 # Scores need flipping so polarity matches other summary metrics
 health_index <-
-  health_index_missing_added |> 
+  health_index_missing_added |>
   mutate(health_index_score = health_index_score * -1)
 
-# ---- Combine and export ----
+# ---- Combine ----
 metrics_joined <-
   ltla |>
   left_join(imd) |>
   left_join(lba) |>
-  left_join(health_index) |> 
-  pivot_longer(cols = !starts_with("ltla21_"), names_to = "variable") |> 
-  select(-ltla21_code) |> 
-  mutate(ltla21_name = str_replace_all(ltla21_name, "'", "")) |> 
+  left_join(health_index) |>
+  pivot_longer(cols = !starts_with("ltla21_"), names_to = "variable") |>
+  select(-ltla21_code) |>
+  mutate(ltla21_name = str_replace_all(ltla21_name, "'", "")) |>
   rename(area_name = ltla21_name)
 
-# Median absolute deviation cannot be used as if more than 50 % of values are
-# identical, then all values are zero. Given that all of the metrics use some
-# kind of score an there are no outliers. Perhaps simply normalising the
-# distributions to mean = 0, SD = 1 is adequate? No, as lba_percentage has a long
-# right tail. Need to find a distribution that is equal in range for each indicator
-metrics_joined |> 
-  group_by(variable) |> 
-  mutate(stand = standardise(value)) |> 
-  filter(variable == "lba_percentage") |> 
-  ggplot(aes(x = stand)) +
-  geom_density()
+# ---- Normalise/scale ----
+scale_1_1 <- function(x) {
+  (x - mean(x)) / max(abs(x - mean(x)))
+}
 
-# Pivot longer. Required cols: area_name, variable, value. Check deprecated code
-# (old commits) for info on cleaning on area names to match other data sets
+ltla_summary_metrics_england <-
+  metrics_joined |>
+  group_by(variable) |>
+  mutate(value = scale_1_1(value))
+
+# Check distributions
+ltla_summary_metrics_england |>
+  ggplot(aes(x = value, y = variable)) +
+  geom_density_ridges(scale = 4) +
+  scale_y_discrete(expand = c(0, 0)) + # will generally have to set the `expand` option
+  scale_x_continuous(expand = c(0, 0)) + # for both axes to remove unneeded padding
+  coord_cartesian(clip = "off") + # to avoid clipping of the very top of the top ridgeline
+  theme_ridges()
 
 usethis::use_data(ltla_summary_metrics_england, overwrite = TRUE)
