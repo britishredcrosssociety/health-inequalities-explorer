9dcbb28e105602853d45718edca1f04e7e9c1cf6
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2022-10-03 16:47:15 +0200

    Add plot template

--- R/agePlot.R
+++ R/agePlot.R
@@ -4,10 +4,36 @@ agePlotUI <- function(id) {
   )
 }
 
-agePlotServer <- function(id) {
-  moduleServer(id, function(input, output, session){
+agePlotServer <- function(id, selected) {
+  moduleServer(id, function(input, output, session) {
+    dataset <- reactive({
+      ltla_demographics_age_england
+    })
+
     output$plot <- renderPlot({
-      
+      dataset() |>
+        dplyr::filter(area_name == "Hartlepool" | area_name == "Rochdale" | area_name == "Bury") |>
+        ggplot(aes(x = population_relative, y = age, fill = area_name)) +
+        facet_wrap(vars(sex), strip.position = "top") +
+        geom_col(position = "dodge", colour = "black", alpha = .7) +
+        geom_text(
+          aes(label = population_label),
+          position = position_dodge(width = .9),
+          hjust = 1.35,
+          # fontface = "bold",
+          colour = "#262626"
+        ) +
+        scale_fill_manual(
+          values = c("#D0021B", "#40A22A", "#F1B13B")
+        ) +
+        scale_x_continuous(labels = scales::percent) +
+        labs(x = "Area population", y = NULL) +
+        theme_minimal() +
+        theme(
+          legend.position = "top",
+          legend.title = element_blank(),
+          text = element_text(face = "bold", size = 15)
+        )
     })
   })
 }
@@ -18,7 +44,10 @@ agePlotTest <- function() {
   )
 
   server <- function(input, output, session) {
-    agePlotServer("test")
+    selected <- reactiveValues(
+      areas = vector(), geography = "ltla_shp_england"
+    )
+    agePlotServer("test", selected)
   }
 
   shinyApp(ui, server)
