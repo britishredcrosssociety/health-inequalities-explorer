bb50c3100f0bfa4cf0962eeebf3478c5d91d0bac
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2022-10-04 14:22:17 +0200

    Refactor to create grey default plot

--- R/jitterPlot.R
+++ R/jitterPlot.R
@@ -17,14 +17,45 @@ jitterPlotServer <- function(id, selected, type) {
     })
 
     output$plot <- renderPlot({
-      # Set sensible plot/legend default if no area has been selected
-      if (is.null(selected$areas)) {
-        dataset <- dataset() |>
-          dplyr::mutate(selected = "not selected") |>
-          dplyr::mutate(alpha = 0.2)
-        legend_break_name <- NULL
-      } else {
-        dataset <- dataset() |>
+      if (is.null(selected$areas) & type == "demographics_age") {
+        dataset() |>
+          ggplot(aes(x = population_relative, y = age)) +
+          geom_point(
+            position = position_jitter(height = 0.25, width = 0.1, seed = 123),
+            size = 5,
+            shape = 21,
+            alpha = 0.1,
+            fill = "#717171",
+            colour = "#262626",
+          ) +
+          scale_x_continuous(labels = scales::percent) +
+          theme_minimal() +
+          labs(x = NULL, y = NULL) +
+          theme(text = element_text(face = "bold", size = 15))
+      } else if (is.null(selected$areas) & type == "summary_metrics") {
+        dataset() |>
+          ggplot(aes(x = value, y = variable)) +
+          geom_vline(
+            xintercept = 0,
+            size = 1,
+            alpha = .7,
+            colour = "#262626",
+            linetype = "dashed"
+          ) +
+          geom_point(
+            position = position_jitter(height = 0.25, width = 0.1, seed = 123),
+            size = 5,
+            shape = 21,
+            alpha = 0.1,
+            fill = "#717171",
+            colour = "#262626",
+          ) +
+          annotate("text", x = 0, y = 3.75, label = "bold(Mean)", parse = TRUE) +
+          theme_minimal() +
+          labs(x = NULL, y = NULL) +
+          theme(text = element_text(face = "bold", size = 15))
+      } else if (type == "demographics_age") {
+        dataset() |>
           dplyr::mutate(
             selected = dplyr::if_else(
               area_name %in% selected$areas,
@@ -36,13 +67,7 @@ jitterPlotServer <- function(id, selected, type) {
             alpha = dplyr::if_else(selected != "not selected", 1, 0.1)
           ) |>
           dplyr::mutate(selected = factor(selected)) |>
-          dplyr::mutate(selected = relevel(selected, ref = "not selected"))
-        legend_break_name <- selected$areas
-      }
-
-      # Create plot objects
-      if (type == "demographics_age") {
-        dataset |>
+          dplyr::mutate(selected = relevel(selected, ref = "not selected")) |>
           ggplot(aes(x = population_relative, y = age, fill = selected)) +
           geom_point(
             aes(alpha = alpha),
@@ -59,14 +84,25 @@ jitterPlotServer <- function(id, selected, type) {
           ) +
           scale_fill_manual(
             values = c("#D0021B", "#40A22A", "#F1B13B", "#6A9EAA"),
-            breaks = legend_break_name
+            breaks = selected$areas
           ) +
           scale_alpha(guide = "none") +
           labs(x = NULL, y = NULL) +
           theme(text = element_text(face = "bold", size = 15))
       } else if (type == "summary_metrics") {
-        # - Summary Metrics
-        dataset |>
+        dataset() |>
+          dplyr::mutate(
+            selected = dplyr::if_else(
+              area_name %in% selected$areas,
+              area_name,
+              "not selected"
+            )
+          ) |>
+          dplyr::mutate(
+            alpha = dplyr::if_else(selected != "not selected", 1, 0.1)
+          ) |>
+          dplyr::mutate(selected = factor(selected)) |>
+          dplyr::mutate(selected = relevel(selected, ref = "not selected")) |>
           ggplot(aes(x = value, y = variable, fill = selected)) +
           geom_vline(
             xintercept = 0,
@@ -90,7 +126,7 @@ jitterPlotServer <- function(id, selected, type) {
           ) +
           scale_fill_manual(
             values = c("#D0021B", "#40A22A", "#F1B13B", "#6A9EAA"),
-            breaks = legend_break_name
+            breaks = selected$areas
           ) +
           scale_alpha(guide = "none") +
           labs(x = NULL, y = NULL) +
