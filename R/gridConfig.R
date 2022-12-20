6825dde9de76bb62db4da7b18b1b5e252c64e5d0
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2022-12-20 20:55:06 +0100

    Tweak layout

--- R/gridConfig.R
+++ R/gridConfig.R
@@ -1,32 +1,33 @@
-main_layout <- c(
-  "1rem   1fr          500px            500px            1fr         ",
-  "500px  github       header           header           bookmark    ",
-  "150px  left_margin  map              select_geography right_margin",
-  "150px  left_margin  map              select_areas     right_margin",
-  "350px  left_margin  summary_metrics  summary_metrics  right_margin",
-  "350px  left_margin  secondary_care   secondary_care   right_margin",
-  "850px  left_margin  demographics     demographics     right_margin"
-)
-
-mobile_layout <- c(
-  "1rem  5px         1fr              5px         ",
-  "120px left_margin header           right_margin",
-  "100px left_margin bookmark         right_margin",
-  "100px left_margin github           right_margin",
-  "100px left_margin select_geography right_margin",
-  "100px left_margin select_areas     right_margin",
-  "1fr   left_margin map              right_margin",
-  "1fr   left_margin summary_metrics  right_margin",
-  "1fr   left_margin secondary_care   right_margin",
-  "1fr   left_margin demographics     right_margin"
-)
-
 grid_config <- new_gridlayout(
-  main_layout,
-  alternate_layouts = list(
-    list(
-      layout = mobile_layout,
-      width_bounds = c(max = 600)
-    )
+  c(
+    "1rem   1fr          250px            250px            250px            250px            1fr         ",
+    "500px  blank1       blank1           header           header           blank2           blank2      ",
+    "175px  left_margin  select_geography select_geography map              map              right_margin",
+    "175px  left_margin  select_areas     select_areas     map              map              right_margin",
+    "350px  left_margin  summary_metrics  summary_metrics  summary_metrics  summary_metrics  right_margin",
+    "350px  left_margin  secondary_care   secondary_care   secondary_care   secondary_care   right_margin",
+    "850px  left_margin  demographics     demographics     demographics     demographics     right_margin"
   )
 )
+# mobile_layout <- c(
+#   "1rem  5px         1fr              5px         ",
+#   "120px left_margin header           right_margin",
+#   "100px left_margin bookmark         right_margin",
+#   "100px left_margin github           right_margin",
+#   "100px left_margin select_geography right_margin",
+#   "100px left_margin select_areas     right_margin",
+#   "1fr   left_margin map              right_margin",
+#   "1fr   left_margin summary_metrics  right_margin",
+#   "1fr   left_margin secondary_care   right_margin",
+#   "1fr   left_margin demographics     right_margin"
+# )
+
+# grid_config <- new_gridlayout(
+#   main_layout,
+#   alternate_layouts = list(
+#     list(
+#       layout = mobile_layout,
+#       width_bounds = c(max = 600)
+#     )
+#   )
+# )
