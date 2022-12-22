7934ef226618f1badc28df611ded762d0f907712
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2022-12-22 15:22:19 +0100

    Set text to auto

--- R/gridConfig.R
+++ R/gridConfig.R
@@ -1,10 +1,11 @@
 grid_config <- new_gridlayout(
   c(
     "1rem   1fr          250px            250px            250px            250px            1fr         ",
-    "80px   github       header           header           header           header           bookmark    ",
-    "500px  left_margin  blank1           intro            intro            blank2           right_margin",
+    "auto   github       header           header           header           header           bookmark    ",
+    "auto   left_margin  blank1           intro            intro            blank2           right_margin",
     "175px  left_margin  select_geography select_geography map              map              right_margin",
     "175px  left_margin  select_areas     select_areas     map              map              right_margin",
+    "auto   left_margin  blank3           summary_intro    summary_intro    blank4           right_margin",
     "350px  left_margin  summary_metrics  summary_metrics  summary_metrics  summary_metrics  right_margin",
     "350px  left_margin  secondary_care   secondary_care   secondary_care   secondary_care   right_margin",
     "850px  left_margin  demographics     demographics     demographics     demographics     right_margin"
