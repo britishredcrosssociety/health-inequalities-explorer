43837e2ff8ae63b57fbb878e784ecfbbdf9a899c
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2023-02-23 14:16:15 +0100

    Remove call to shiny

--- R/utils.R
+++ R/utils.R
@@ -1,7 +1,7 @@
 # Currently, `grid_card()` can only be collapsed using the arrow icon.
 # This function also allows cards to be collapsed by clicking their title.
 title_collapsible <- function(title) {
-  shiny::span(
+  span(
     "onclick" = '
     var card = this.parentElement.parentElement;
     var card_classes = card.classList;
