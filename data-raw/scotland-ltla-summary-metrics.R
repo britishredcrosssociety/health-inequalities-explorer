0180e3d1575b9438ee5159a16179571a88c1f516
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2023-05-09 11:02:29 +0200

    Add Scotland ltla summary indicators

---
+++ data-raw/scotland-ltla-summary-metrics.R
@@ -0,0 +1,104 @@
+library(tidyverse)
+library(IMD)
+library(geographr)
+library(sf)
+library(ggridges)
+
+ltla <- boundaries_ltla21 |>
+  st_drop_geometry() |>
+  filter(str_detect(ltla21_code, "^S"))
+
+lookup_iz_ltla <- lookup_dz11_iz11_ltla20 |>
+  select(iz11_code, ltla21_code = ltla20_code) |>
+  distinct()
+
+# ---- IMD score ----
+# Higher extent = more deprived
+# Higher rank (calculated here) = more deprived
+imd <-
+  imd_scotland_lad |>
+  select(ltla21_code = lad_code, imd_score = Extent) |>
+  mutate(number = rank(imd_score)) |>
+  select(-imd_score) |>
+  mutate(variable = "Index of Multiple \nDeprivation rank", .after = ltla21_code) |>
+  mutate(percent = NA, .after = number)
+
+# ---- % Left-behind areas ----
+# Higher number/percent = more left-behind
+lba <-
+  cni_scotland_iz11 |>
+  left_join(lookup_iz_ltla) |>
+  select(iz11_code, ltla21_code, lba = `Left Behind Area?`) |>
+  group_by(ltla21_code) |>
+  count(lba) |>
+  mutate(percent = n / sum(n)) |>
+  ungroup() |>
+  filter(lba == TRUE) |>
+  right_join(ltla) |>
+  mutate(percent = replace_na(percent, 0)) |>
+  mutate(n = replace_na(n, 0)) |>
+  select(ltla21_code, number = n, percent) |>
+  mutate(variable = "Left-behind areas", .after = ltla21_code)
+
+# ---- Health Index Score ----
+# An official Health Index for Scotland does not exists. Use the BRC Resilience
+# Index version
+# Higher score = worse health
+# Higher rank (calculated here) = worse health
+health_index_raw <- read_csv(
+  "https://raw.githubusercontent.com/britishredcrosssociety/resilience-index/main/data/vulnerability/health-inequalities/scotland/index-unweighted-all-indicators.csv"
+)
+
+health_index <- health_index_raw |>
+  select(ltla21_code = lad_code, number = health_inequalities_composite_rank) |>
+  mutate(percent = NA) |>
+  mutate(variable = "Health Index \nrank") |>
+  relocate(variable, .after = ltla21_code)
+
+# ---- Combine & reanme (pretty printing) ----
+metrics_joined <- bind_rows(
+  imd,
+  lba,
+  health_index
+) |>
+  left_join(ltla) |>
+  select(-ltla21_code) |>
+  rename(area_name = ltla21_name) |>
+  relocate(area_name) |>
+  mutate(geography_type = "LTLA") |>
+  mutate(data_type = "Summary metrics") |>
+  relocate(geography_type, data_type, .after = area_name)
+
+# ---- Normalise/scale ----
+scale_1_1 <- function(x) {
+  (x - mean(x)) / max(abs(x - mean(x)))
+}
+
+ltla_summary_metrics_scotland_scaled <-
+  metrics_joined |>
+  group_by(variable) |>
+  mutate(
+    scaled_1_1 = case_when(
+      variable == "Index of Multiple \nDeprivation rank" ~ scale_1_1(number),
+      variable == "Left-behind areas" ~ scale_1_1(percent),
+      variable == "Health Index \nrank" ~ scale_1_1(number)
+    )
+  ) |>
+  ungroup()
+
+# ---- Align indicator polarity ----
+# Align so higher value = better health
+# Flip IMD, LBA, and health index, as currently higher = worse health
+ltla_summary_metrics_scotland <- ltla_summary_metrics_scotland_scaled |>
+  mutate(scaled_1_1 = scaled_1_1 * -1)
+
+# Check distributions
+ltla_summary_metrics_scotland |>
+  ggplot(aes(x = scaled_1_1, y = variable)) +
+  geom_density_ridges(scale = 4) +
+  scale_y_discrete(expand = c(0, 0)) +
+  scale_x_continuous(expand = c(0, 0)) +
+  coord_cartesian(clip = "off") +
+  theme_ridges()
+
+usethis::use_data(ltla_summary_metrics_scotland, overwrite = TRUE)
