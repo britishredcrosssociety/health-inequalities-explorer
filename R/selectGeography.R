e44a65794b818f41c6108ab51637d52d50631245
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2023-05-09 09:58:02 +0200

    Hide Scotland shape files: - Currently, no indicator data exists to link to the shape files.

--- R/selectGeography.R
+++ R/selectGeography.R
@@ -4,9 +4,9 @@ selectGeographyUI <- function(id) {
     label = NULL,
     choices = c(
       "England: Local Authorities" = "england_ltla_shp",
-      "England: Integrated Care Boards" = "england_icb_shp",
-      "Scotland: Local Authorities" = "scotland_ltla_shp",
-      "Scotland: Health Boards" = "scotland_hb_shp"
+      "England: Integrated Care Boards" = "england_icb_shp"
+      # "Scotland: Local Authorities" = "scotland_ltla_shp",
+      # "Scotland: Health Boards" = "scotland_hb_shp"
     ),
     multiple = FALSE
   )
