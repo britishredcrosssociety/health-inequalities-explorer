c1c5ce16556273eaab5b0be75dbc8f27da9f4337
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2023-05-12 15:29:31 +0200

    Add Scottish LTLAs

--- R/jitterPlot.R
+++ R/jitterPlot.R
@@ -9,18 +9,24 @@ jitterPlotServer <- function(id, selected, type) {
   moduleServer(id, function(input, output, session) {
     # Select dataset based on geographical selection and type of data
     dataset <- reactive({
-      if (selected$geography == "england_ltla_shp" & type == "demographics") {
-        england_ltla_demographics
-      } else if (selected$geography == "england_ltla_shp" & type == "summary_metrics") {
+      if (selected$geography == "england_ltla_shp" & type == "summary_metrics") {
         england_ltla_summary_metrics
       } else if (selected$geography == "england_ltla_shp" & type == "secondary_care") {
         england_ltla_secondary_care
-      } else if (selected$geography == "england_icb_shp" & type == "demographics") {
-        england_icb_demographics
+      } else if (selected$geography == "england_ltla_shp" & type == "demographics") {
+        england_ltla_demographics
       } else if (selected$geography == "england_icb_shp" & type == "summary_metrics") {
         england_icb_summary_metrics
       } else if (selected$geography == "england_icb_shp" & type == "secondary_care") {
         england_icb_secondary_care
+      } else if (selected$geography == "england_icb_shp" & type == "demographics") {
+        england_icb_demographics
+      } else if (selected$geography == "scotland_ltla_shp" & type == "summary_metrics") {
+        scotland_ltla_summary_metrics
+      } else if (selected$geography == "scotland_ltla_shp" & type == "secondary_care") {
+        scotland_ltla_secondary_care
+      } else if (selected$geography == "scotland_ltla_shp" & type == "demographics") {
+        scotland_ltla_demographics
       }
     })
 
