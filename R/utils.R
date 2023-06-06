72d4826085eb48ce8e52ad86bdb6a83accdcb5c1
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2023-06-06 18:43:13 +0200

    Wrap plot labels: - prevent plots from squishing due to long legend labels

--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,5 @@
-# Currently, `grid_card()` can only be collapsed using the arrow icon.
-# This function also allows cards to be collapsed by clicking their title.
+#' Currently, `grid_card()` can only be collapsed using the arrow icon.
+#' This function also allows cards to be collapsed by clicking their title.
 title_collapsible <- function(title) {
   span(
     "onclick" = '
@@ -15,3 +15,10 @@ title_collapsible <- function(title) {
     title
   )
 }
+
+#' Wrap strings in the jitter plot
+string_wrap_unvectorized <- function(string) {
+  paste(strwrap(string, width = 20), collapse = "\n")
+}
+
+string_wrap <- Vectorize(string_wrap_unvectorized, USE.NAMES = FALSE)
