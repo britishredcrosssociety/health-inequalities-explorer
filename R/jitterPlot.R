0a1dce862e35466b2f3a66683bfb9122c0ae94bf
jennajt <jennajltan@gmail.com>
2023-07-03 16:32:57 +0100

    v1 switch statement replacement

--- R/jitterPlot.R
+++ R/jitterPlot.R
@@ -9,32 +9,61 @@ jitterPlotServer <- function(id, selected, type) {
   moduleServer(id, function(input, output, session) {
     # Select dataset based on geographical selection and type of data
     dataset <- reactive({
-      if (selected$geography == "england_ltla_shp" & type == "summary_metrics") {
-        england_ltla_summary_metrics
-      } else if (selected$geography == "england_ltla_shp" & type == "secondary_care") {
-        england_ltla_secondary_care
-      } else if (selected$geography == "england_ltla_shp" & type == "demographics") {
-        england_ltla_demographics
-      } else if (selected$geography == "england_icb_shp" & type == "summary_metrics") {
-        england_icb_summary_metrics
-      } else if (selected$geography == "england_icb_shp" & type == "secondary_care") {
-        england_icb_secondary_care
-      } else if (selected$geography == "england_icb_shp" & type == "demographics") {
-        england_icb_demographics
-      } else if (selected$geography == "scotland_ltla_shp" & type == "summary_metrics") {
-        scotland_ltla_summary_metrics
-      } else if (selected$geography == "scotland_ltla_shp" & type == "secondary_care") {
-        scotland_ltla_secondary_care
-      } else if (selected$geography == "scotland_ltla_shp" & type == "demographics") {
-        scotland_ltla_demographics
-      } else if (selected$geography == "scotland_hb_shp" & type == "summary_metrics") {
-        scotland_hb_summary_metrics
-      } else if (selected$geography == "scotland_hb_shp" & type == "secondary_care") {
-        scotland_hb_secondary_care
-      } else if (selected$geography == "scotland_hb_shp" & type == "demographics") {
-        scotland_hb_demographics
-      } 
+      if (selected$geography == "england_ltla_shp") {
+        switch(type,
+               "summary_metrics" = england_ltla_summary_metrics,
+               "secondary_care" = england_ltla_secondary_care,
+               "demographics" = england_ltla_demographics
+        )
+      } else if (selected$geography == "england_icb_shp") {
+        switch(type,
+               "summary_metrics" = england_icb_summary_metrics,
+               "secondary_care" = england_icb_secondary_care,
+               "demographics" = england_icb_demographics
+        )
+      } else if (selected$geography == "scotland_ltla_shp") {
+        switch(type,
+               "summary_metrics" = scotland_ltla_summary_metrics,
+               "secondary_care" = scotland_ltla_secondary_care,
+               "demographics" = scotland_ltla_demographics
+        )
+      } else if (selected$geography == "scotland_hb_shp") {
+        switch(type,
+               "summary_metrics" = scotland_hb_summary_metrics,
+               "secondary_care" = scotland_hb_secondary_care,
+               "demographics" = scotland_hb_demographics
+        )
+      }
     })
+    
+    
+    # dataset <- reactive({
+    #   if (selected$geography == "england_ltla_shp" & type == "summary_metrics") {
+    #     england_ltla_summary_metrics
+    #   } else if (selected$geography == "england_ltla_shp" & type == "secondary_care") {
+    #     england_ltla_secondary_care
+    #   } else if (selected$geography == "england_ltla_shp" & type == "demographics") {
+    #     england_ltla_demographics
+    #   } else if (selected$geography == "england_icb_shp" & type == "summary_metrics") {
+    #     england_icb_summary_metrics
+    #   } else if (selected$geography == "england_icb_shp" & type == "secondary_care") {
+    #     england_icb_secondary_care
+    #   } else if (selected$geography == "england_icb_shp" & type == "demographics") {
+    #     england_icb_demographics
+    #   } else if (selected$geography == "scotland_ltla_shp" & type == "summary_metrics") {
+    #     scotland_ltla_summary_metrics
+    #   } else if (selected$geography == "scotland_ltla_shp" & type == "secondary_care") {
+    #     scotland_ltla_secondary_care
+    #   } else if (selected$geography == "scotland_ltla_shp" & type == "demographics") {
+    #     scotland_ltla_demographics
+    #   } else if (selected$geography == "scotland_hb_shp" & type == "summary_metrics") {
+    #     scotland_hb_summary_metrics
+    #   } else if (selected$geography == "scotland_hb_shp" & type == "secondary_care") {
+    #     scotland_hb_secondary_care
+    #   } else if (selected$geography == "scotland_hb_shp" & type == "demographics") {
+    #     scotland_hb_demographics
+    #   } 
+    # })
 
     output$plot <- renderPlotly({
       if (is.null(selected$areas)) {
