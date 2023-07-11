e03823635474d76296c6f870ef5e8bacdbfc0be3
Msdimple <a.c.diallo@smd22.qmul.ac.uk>
2023-07-11 13:59:12 +0000

    Update on Wales summary metrics

--- data-raw/wales-ltla-summary-metrics.R
+++ data-raw/wales-ltla-summary-metrics.R
@@ -3,6 +3,8 @@ library(IMD)
 library(geographr)
 library(sf)
 library(ggridges)
+
+
 ltla <- boundaries_ltla21 |>
   st_drop_geometry() |>
   filter(str_detect(ltla21_code, "^W"))
@@ -20,10 +22,10 @@ imd <-
   mutate(percent = NA, .after = number)
 #### ---- % Left-behind areas ----
 # Higher number/percent = more left-behind
-View(cni_wales_msoa11)
+
 lba <-
   cni_wales_msoa11 |>
-  rename(ltla21_code=msoa11_code) |>
+  rename(ltla21_code=msoa11_code) |>######### I rename because I don't have ltla21_code on the dataset
   select(ltla21_code, lad21_name, lba = `Left Behind Area?`) |>
   group_by(lad21_name) |>
   count(lba) |>
@@ -40,11 +42,77 @@ lba <-
 # Index version
 # Higher score = worse health
 # Higher rank  = worse health
-health_index_raw <- read_csv(
-  "https://github.com/britishredcrosssociety/resilience-index/blob/main/data/vulnerability/health-inequalities/wales/healthy-people-domain.csv")
-View (health_index_raw)
+url <- "https://raw.githubusercontent.com/britishredcrosssociety/resilience-index/main/data/vulnerability/health-inequalities/wales/healthy-people-domain.csv"
+health_index_raw <- read_csv(url)
 health_index <- health_index_raw |>
-  select(ltla21_code = lad_code, number =healthy_people_domain_rank) |>
+  rename(ltla21_code=lad_code) |>
+  select(ltla21_code, number =healthy_people_domain_rank) |>
   mutate(percent = NA) |>
   mutate(variable = "Health Index \nrank") |>
   relocate(variable, .after = ltla21_code)
+# ---- Combine & reanme (pretty printing) ----
+metrics_joined <- bind_rows(
+  imd,
+  lba,
+  health_index
+) |>
+  left_join(ltla) |>
+  select(-ltla21_code) |>
+  rename(area_name = ltla21_name) |>
+  relocate(area_name)
+View(metrics_joined)
+# ---- Normalise/scale ----
+scale_1_1 <- function(x) {
+  (x - mean(x)) / max(abs(x - mean(x)))
+}
+
+ltla_summary_metrics_wales_scaled <-
+  metrics_joined |>
+  group_by(variable) |>
+  mutate(
+    scaled_1_1 = case_when(
+      variable == "Index of Multiple \nDeprivation rank" ~ scale_1_1(number),
+      variable == "Left-behind areas" ~ scale_1_1(percent),
+      variable == "Health Index \nrank" ~ scale_1_1(number)
+    )
+  ) |>
+  ungroup()
+
+# ---- Align indicator polarity ----
+# Align so higher value = better health
+# Flip IMD, LBA, and health index, as currently higher = worse health
+wales_ltla_summary_metrics_polarised <- ltla_summary_metrics_wales_scaled |>
+  mutate(scaled_1_1 = scaled_1_1 * -1)
+
+# Check distributions
+wales_ltla_summary_metrics_polarised |>
+  ggplot(aes(x = scaled_1_1, y = variable)) +
+  geom_density_ridges(scale = 4) +
+  scale_y_discrete(expand = c(0, 0)) +
+  scale_x_continuous(expand = c(0, 0)) +
+  coord_cartesian(clip = "off") +
+  theme_ridges()
+
+# ---- Add plot labels ----
+wales_ltla_summary_metrics <- wales_ltla_summary_metrics_polarised |>
+  mutate(
+    label = case_when(
+      variable == "Index of Multiple \nDeprivation rank" ~ paste0(
+        "<b>", area_name, "</b>",
+        "<br>",
+        "<br>", "IMD rank: ", round(number)
+      ),
+      variable == "Left-behind areas" ~ paste0(
+        "<b>", area_name, "</b>",
+        "<br>",
+        "<br>", "No. of left-behind Intermediate Zones in the Local Authority: ", round(number),
+        "<br>", "Percentage of all left-behind Intermediate Zones in the Local Authority: ", round(percent * 100, 1), "%"
+      ),
+      variable == "Health Index \nrank" ~ paste0(
+        "<b>", area_name, "</b>",
+        "<br>",
+        "<br>", "Health Index rank: ", round(number)
+      )
+    )
+  )
+usethis::use_data(wales_ltla_summary_metrics, overwrite = TRUE)
