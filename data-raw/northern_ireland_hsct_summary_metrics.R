c1b291c986808e1eee601ae569409f6fae116263
MatteoLarrode <matteo.larrode@orange.fr>
2023-07-12 13:01:20 +0100

    aggregated lba's in data-raw NI hsct

--- data-raw/northern_ireland_hsct_summary_metrics.R
+++ data-raw/northern_ireland_hsct_summary_metrics.R
@@ -11,10 +11,13 @@ lookup_northern_ireland_ltla_hsct <-
   lookup_ltla21_hsct18 |> 
   select(lad_name = ltla21_name, trust_name = trust18_name)
 
-# ---- Aggregate Local Authorities measures ----
+list_northern_ireland_hsct <- 
+  lookup_northern_ireland_ltla_hsct |> 
+  distinct(trust_name)
 
+# ---- Aggregate Local Authorities measures ----
 # Metrics where rank is used: aggregate with maximum rank
-imd_health_hsct <-
+imd_health <-
   northern_ireland_ltla_summary_metrics |> 
   select(lad_name = area_name, variable, number) |> 
   filter(variable %in% 
@@ -30,13 +33,28 @@ imd_health_hsct <-
   ungroup()
 
 # Left-behind areas: aggregate by adding all left-behind areas of LGDs
-lba_hsct <-
-  northern_ireland_ltla_summary_metrics |> 
-  select(lad_name = area_name, variable, number, percent) |> 
-  filter(variable == "Left-behind areas") |> 
+lba <-
+  cni_northern_ireland_soa11 |>
+  select(soa11_code, lad_name = lgd14_name, lba = `Left Behind Area?`) |> 
   left_join(lookup_northern_ireland_ltla_hsct) |> 
-  group_by(trust_name, variable) |> 
-  summarise(number = sum(number))
+  group_by(trust_name) |>
+  count(lba) |>
+  mutate(percent = n / sum(n)) |> 
+  ungroup() |>
+  filter(lba == TRUE) |>
+  right_join(list_northern_ireland_hsct) |> 
+  mutate(percent = replace_na(percent, 0)) |>
+  mutate(n = replace_na(n, 0)) |>
+  select(trust_name, number = n, percent) |>
+  mutate(variable = "Left-behind areas", .after = trust_name)
+
+# ---- Combine & rename (pretty printing) ----
+metrics_joined <- bind_rows(
+  imd_health,
+  lba
+) |>
+  rename(area_name = trust_name) |>
+  relocate(area_name)
 
 
 
