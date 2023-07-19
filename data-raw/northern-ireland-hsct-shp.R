240e7160de10e8be3254513558403c4e5fb89aef
niuni02 <111527060+niuni02@users.noreply.github.com>
2023-07-19 15:15:41 +0100

    Removed .Rproj file

--- data-raw/northern-ireland-hsct-shp.R
+++ data-raw/northern-ireland-hsct-shp.R
@@ -8,4 +8,4 @@ northern_ireland_hsct_shp <-
   ms_simplify(keep = 0.5) |> # Any less and areas get dropped
   select(area_name = trust18_name, area_code = trust18_code)
 
-usethis::use_data(england_ltla_shp, overwrite = TRUE)
+usethis::use_data(northern_ireland_hsct_shp, overwrite = TRUE)
--- data-raw/northern-ireland-lgd-shp.R
+++ data-raw/northern-ireland-lgd-shp.R
@@ -9,4 +9,4 @@ northern_ireland_lgd_shp <-
   ms_simplify(keep = 0.5) |> # Any less and areas get dropped
   select(area_name = ltla21_name, area_code = ltla21_code)
 
-usethis::use_data(england_ltla_shp, overwrite = TRUE)
+usethis::use_data(northern_ireland_lgd_shp, overwrite = TRUE)
