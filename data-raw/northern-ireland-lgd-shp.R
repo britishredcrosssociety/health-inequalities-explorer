5933428827396e91cdf5ed19bc48861958966382
niuni02 <111527060+niuni02@users.noreply.github.com>
2023-07-19 10:55:33 +0100

    NI shp file from lgd view

---
+++ data-raw/northern-ireland-lgd-shp.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+library(sf)
+library(geographr)
+library(rmapshaper)
+
+northern_ireland_lgd_shp <-
+  boundaries_ltla21 |>
+  filter(str_detect(ltla21_code, "^N")) |>
+  ms_simplify(keep = 0.5) |> # Any less and areas get dropped
+  select(area_name = ltla21_name, area_code = ltla21_code)
+
+usethis::use_data(england_ltla_shp, overwrite = TRUE)
