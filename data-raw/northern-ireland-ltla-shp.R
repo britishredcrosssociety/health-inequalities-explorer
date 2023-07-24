502732812d084e7076e6beceba52e8f598f9c280
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2023-07-24 18:56:29 +0200

    Unsimplify shape for clearer boundaries

--- data-raw/northern-ireland-ltla-shp.R
+++ data-raw/northern-ireland-ltla-shp.R
@@ -6,7 +6,7 @@ library(rmapshaper)
 northern_ireland_ltla_shp <-
   boundaries_ltla21 |>
   filter(str_detect(ltla21_code, "^N")) |>
-  ms_simplify(keep = 0.05) |> 
+  ms_simplify(keep = 0.5) |>
   select(area_name = ltla21_name, area_code = ltla21_code)
 
 usethis::use_data(northern_ireland_ltla_shp, overwrite = TRUE)
