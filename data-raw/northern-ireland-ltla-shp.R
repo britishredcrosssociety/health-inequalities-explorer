91fe330bc08d2bb3932479df26028b2555de1002
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2023-07-24 17:54:31 +0200

    Rename to use ltla suffix

--- data-raw/northern-ireland-ltla-shp.R
+++ data-raw/northern-ireland-ltla-shp.R
@@ -3,10 +3,10 @@ library(sf)
 library(geographr)
 library(rmapshaper)
 
-northern_ireland_lgd_shp <-
+northern_ireland_ltla_shp <-
   boundaries_ltla21 |>
   filter(str_detect(ltla21_code, "^N")) |>
-  ms_simplify(keep = 0.5) |> # Any less and areas get dropped
+  ms_simplify(keep = 0.05) |> 
   select(area_name = ltla21_name, area_code = ltla21_code)
 
-usethis::use_data(northern_ireland_lgd_shp, overwrite = TRUE)
+usethis::use_data(northern_ireland_ltla_shp, overwrite = TRUE)
