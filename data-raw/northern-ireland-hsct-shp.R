4a492c7d3029f6ea5423f6d0219154944aa9be2b
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2023-07-24 17:54:42 +0200

    Simplify shape to reduce load times

--- data-raw/northern-ireland-hsct-shp.R
+++ data-raw/northern-ireland-hsct-shp.R
@@ -5,7 +5,7 @@ library(rmapshaper)
 
 northern_ireland_hsct_shp <-
   boundaries_trusts_ni18 |>
-  ms_simplify(keep = 0.5) |> # Any less and areas get dropped
+  ms_simplify(keep = 0.05) |>
   select(area_name = trust18_name, area_code = trust18_code)
 
 usethis::use_data(northern_ireland_hsct_shp, overwrite = TRUE)
