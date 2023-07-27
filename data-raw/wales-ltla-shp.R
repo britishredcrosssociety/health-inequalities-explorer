d463595ffa428e32a8327d172e8e0b866e732225
Msdimple <a.c.diallo@smd22.qmul.ac.uk>
2023-07-27 16:12:44 +0000

    Wales ltla shape

---
+++ data-raw/wales-ltla-shp.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+library(sf)
+library(geographr)
+library(rmapshaper)
+
+wales_ltla_shp <-
+  boundaries_ltla21 |>
+  filter(str_detect(ltla21_code, "^W")) |>
+  ms_simplify(keep = 0.5) |> 
+  select(area_name = ltla21_name, area_code = ltla21_code)
+
+usethis::use_data(wales_ltla_shp, overwrite = TRUE)
