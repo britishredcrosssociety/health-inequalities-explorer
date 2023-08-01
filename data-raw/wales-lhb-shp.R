c3a3f5867028b5393d2a0a2d10bc5fac5c23b2cd
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2023-08-01 15:30:24 +0200

    Remove call to mapshaper

--- data-raw/wales-lhb-shp.R
+++ data-raw/wales-lhb-shp.R
@@ -4,7 +4,6 @@ library(sf)
 
 wales_lhb_shp <-
   boundaries_lhb20 |>
-  ms_simplify(keep = 0.5) |> 
   select(area_name = lhb20_name, area_code = lhb20_code)
 
 usethis::use_data(wales_lhb_shp, overwrite = TRUE)
