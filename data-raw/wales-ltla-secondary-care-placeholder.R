6ac6bf11dfd53303109239884950ae8795e4275a
MatteoLarrode <matteo.larrode@orange.fr>
2023-08-28 14:58:24 +0200

    improved  placeholder dataset

--- data-raw/wales-ltla-secondary-care-placeholder.R
+++ data-raw/wales-ltla-secondary-care-placeholder.R
@@ -1,17 +1,24 @@
 # Placeholder dataset for Wales LTLA secondary care data
 wales_ltla_secondary_care_placeholder <- tibble(
-  variable = c(
-    "Bed availability \n(Mar 2022)",
-    "Referral to treatment \nwaiting times (Jul 22 - Sep 22)"
+  area_name = rep(
+    c(
+      "Isle of Anglesey", "Gwynedd", "Conwy", "Denbighshire", "Flintshire",
+      "Wrexham", "Ceredigion", "Pembrokeshire", "Carmarthenshire", "Swansea",
+      "Neath Port Talbot", "Bridgend", "Vale of Glamorgan", "Cardiff",
+      "Rhondda Cynon Taf", "Caerphilly", "Blaenau Gwent", "Torfaen",
+      "Monmouthshire", "Newport", "Powys", "Merthyr Tydfil"
+    ),
+    2
   ),
-  scaled_1_1 = c(
-    NA,
-    NA
+  variable = rep(
+    c(
+      "Bed availability \n(Mar 2022)",
+      "Referral to treatment \nwaiting times (Jul 22 - Sep 22)"
+    ),
+    22
   ),
-  label = c(
-    NA,
-    NA
-  )
+  scaled_1_1 = NA,
+  label = NA
 )
 
 usethis::use_data(wales_ltla_secondary_care_placeholder, overwrite = TRUE)
