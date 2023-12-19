1fa7836223324ca31fab9191deadda30da4e421a
Matteo Larrode <matteo.larrode@gmail.com>
2023-12-19 15:35:50 +0100

    indicator query working

--- R/indicatorDescriptions.R
+++ R/indicatorDescriptions.R
@@ -8,52 +8,10 @@ indicatorDescriptionsServer <- function(id, selected, type) {
       # ---- england_ltla_summary_metrics ----
       if (selected$geography == "england_ltla_shp" & type == "summary_metrics") {
         tagList(
-          tags$p(
-            "These indicators summarise a selection of health metrics into a single
-        score. They can be useful for comparing the overall health of different
-        areas and are a good place to start. But, they should not be used in
-        isolation to make judgements about all aspects of an area's health. For
-        example, an area may score poorly in a summary metric, yet still excel
-        in certain aspects of health."
-          ),
-          tags$p(
-            "The ONS Health Index provides an indication of health outcomes, risk
-        factors, and the wider determinants of health. A detailed breakdown
-        of the index can be viewed ",
-            tags$a(
-              href = "https://www.ons.gov.uk/peoplepopulationandcommunity/healthandsocialcare/healthandwellbeing/bulletins/healthinengland/2015to2021",
-              target = "_blank",
-              "here."
-            )
-          ),
-          tags$p(
-            "Left-behind areas are places high in deprivation and socio-economic
-        challenges, and low in social infrastructure and investment to meet those
-        challenges. Research has shown they are associated with high health
-        inequalities. More information on these areas can be found ",
-            tags$a(
-              href = "https://ocsi.uk/left-behind-neighbourhoods/",
-              target = "_blank",
-              "here."
-            ),
-            " An interactive map to visualise these areas can be found",
-            tags$a(
-              href = "https://britishredcross.shinyapps.io/left-behind-areas/",
-              target = "_blank",
-              "here."
-            )
-          ),
-          tags$p(
-            "The Indices of Multiple Deprivation (IMD) include a measure of health
-        that measures the risk of premature death and the impairment of quality
-        of life through poor physical or mental health. More information can be
-        viewed ",
-            tags$a(
-              href = "https://www.gov.uk/government/statistics/english-indices-of-deprivation-2019",
-              target = "_blank",
-              "here."
-            )
-          )
+          list_indicators[list_indicators$id == "summary_intro",]$tag,
+          list_indicators[list_indicators$id == "health_index",]$tag,
+          list_indicators[list_indicators$id == "lba",]$tag,
+          list_indicators[list_indicators$id == "imd_england",]$tag,
         )
 
         # ---- england_ltla_secondary_care ----
@@ -135,52 +93,10 @@ indicatorDescriptionsServer <- function(id, selected, type) {
         # ---- england_icb_summary_metrics ----
       } else if (selected$geography == "england_icb_shp" & type == "summary_metrics") {
         tagList(
-          tags$p(
-            "These indicators summarise a selection of health metrics into a single
-        score. They can be useful for comparing the overall health of different
-        areas and are a good place to start. But, they should not be used in
-        isolation to make judgements about all aspects of an area's health. For
-        example, an area may score poorly in a summary metric, yet still excel
-        in certain aspects of health."
-          ),
-          tags$p(
-            "The ONS Health Index provides an indication of health outcomes, risk
-        factors, and the wider determinants of health. A detailed breakdown
-        of the index can be viewed ",
-            tags$a(
-              href = "https://www.ons.gov.uk/peoplepopulationandcommunity/healthandsocialcare/healthandwellbeing/bulletins/healthinengland/2015to2021",
-              target = "_blank",
-              "here."
-            )
-          ),
-          tags$p(
-            "Left-behind areas are places high in deprivation and socio-economic
-        challenges, and low in social infrastructure and investment to meet those
-        challenges. Research has shown they are associated with high health
-        inequalities. More information on these areas can be found ",
-            tags$a(
-              href = "https://ocsi.uk/left-behind-neighbourhoods/",
-              target = "_blank",
-              "here."
-            ),
-            " An interactive map to visualise these areas can be found",
-            tags$a(
-              href = "https://britishredcross.shinyapps.io/left-behind-areas/",
-              target = "_blank",
-              "here."
-            )
-          ),
-          tags$p(
-            "The Indices of Multiple Deprivation (IMD) include a measure of health
-        that measures the risk of premature death and the impairment of quality
-        of life through poor physical or mental health. More information can be
-        viewed ",
-            tags$a(
-              href = "https://www.gov.uk/government/statistics/english-indices-of-deprivation-2019",
-              target = "_blank",
-              "here."
-            )
-          )
+          list_indicators[list_indicators$id == "summary_intro",]$tag,
+          list_indicators[list_indicators$id == "health_index",]$tag,
+          list_indicators[list_indicators$id == "lba",]$tag,
+          list_indicators[list_indicators$id == "imd_england",]$tag,
         )
 
         # ---- england_icb_secondary_care ----
--- data-raw/list-indicators.R
+++ data-raw/list-indicators.R
@@ -1,66 +1,62 @@
 # ---- Load libraries ----
 library(tibble)
+library(shiny)
 
 # ---- Build data ----
 list_indicators <-
   tribble(
     
-    # Column Names
-    ~geography, ~type, ~description,
+    # ----Column Names----
+    ~id, ~type , ~indicator, ~tag,
     
-    # England
-    "england_ltla_shp", "summary_metrics", tagList(
-      tags$p(
-        "These indicators summarise a selection of health metrics into a single
+    # ----Summary metrics----
+    "summary_intro", "summary_metrics", "intro",  tags$p(
+      "These indicators summarise a selection of health metrics into a single
         score. They can be useful for comparing the overall health of different
         areas and are a good place to start. But, they should not be used in
         isolation to make judgements about all aspects of an area's health. For
         example, an area may score poorly in a summary metric, yet still excel
         in certain aspects of health."
-      ),
-      tags$p(
-        "The ONS Health Index provides an indication of health outcomes, risk
+    ),
+    "health_index", "summary_metrics", "health_index", tags$p(
+      "The ONS Health Index provides an indication of health outcomes, risk
         factors, and the wider determinants of health. A detailed breakdown
         of the index can be viewed ",
-        tags$a(
-          href = "https://www.ons.gov.uk/peoplepopulationandcommunity/healthandsocialcare/healthandwellbeing/bulletins/healthinengland/2015to2021",
-          target = "_blank",
-          "here."
-        )
-      ),
-      tags$p(
-        "Left-behind areas are places high in deprivation and socio-economic
+      tags$a(
+        href = "https://www.ons.gov.uk/peoplepopulationandcommunity/healthandsocialcare/healthandwellbeing/bulletins/healthinengland/2015to2021",
+        target = "_blank",
+        "here."
+      )
+    ),
+    "lba", "summary_metrics", "left_behind_areas", tags$p(
+      "Left-behind areas are places high in deprivation and socio-economic
         challenges, and low in social infrastructure and investment to meet those
         challenges. Research has shown they are associated with high health
         inequalities. More information on these areas can be found ",
-        tags$a(
-          href = "https://ocsi.uk/left-behind-neighbourhoods/",
-          target = "_blank",
-          "here."
-        ),
-        " An interactive map to visualise these areas can be found",
-        tags$a(
-          href = "https://britishredcross.shinyapps.io/left-behind-areas/",
-          target = "_blank",
-          "here."
-        )
+      tags$a(
+        href = "https://ocsi.uk/left-behind-neighbourhoods/",
+        target = "_blank",
+        "here."
       ),
-      tags$p(
-        "The Indices of Multiple Deprivation (IMD) include a measure of health
+      " An interactive map to visualise these areas can be found",
+      tags$a(
+        href = "https://britishredcross.shinyapps.io/left-behind-areas/",
+        target = "_blank",
+        "here."
+      )
+    ),
+    "imd_england", "summary_metrics", "IMD", tags$p(
+      "The Indices of Multiple Deprivation (IMD) include a measure of health
         that measures the risk of premature death and the impairment of quality
         of life through poor physical or mental health. More information can be
         viewed ",
-        tags$a(
-          href = "https://www.gov.uk/government/statistics/english-indices-of-deprivation-2019",
-          target = "_blank",
-          "here."
-        )
+      tags$a(
+        href = "https://www.gov.uk/government/statistics/english-indices-of-deprivation-2019",
+        target = "_blank",
+        "here."
       )
     )
-    ,
-    "england_icb_shp", "summary_metrics", "test2"
-      
-      
+    
   )
 
 
