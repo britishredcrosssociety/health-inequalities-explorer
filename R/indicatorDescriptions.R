d73778d0a79ee0d67d2f9ca59d2c6fef7e9a4367
Matteo Larrode <matteo.larrode@gmail.com>
2023-12-19 15:49:09 +0100

    finished England summary & secondary care

--- R/indicatorDescriptions.R
+++ R/indicatorDescriptions.R
@@ -17,62 +17,10 @@ indicatorDescriptionsServer <- function(id, selected, type) {
         # ---- england_ltla_secondary_care ----
       } else if (selected$geography == "england_ltla_shp" & type == "secondary_care") {
         tagList(
-          tags$p(
-            "Secondary care indicators report on the direct performance of the national
-         health service. Most secondary care statistics are reported only at
-         the Trust level. To be able to view these statistics at the local authority
-         level, we have aggregated them using catchment population data.
-         For more information on how we have done this, see ",
-            tags$a(
-              href = "https://britishredcrosssociety.github.io/resilience-index-book/technical.html#health-capacity---england",
-              target = "_blank",
-              "here."
-            )
-          ),
-          tags$p(
-            "The Improving Access to Pyschological Therapies (IAPT) programme offers
-        talking therapies for mental health problems. To address both access to
-        mental health services and the success rate of their interventions, the
-        percentage of referrals that were able to access a service within 18
-        weeks and also finished the first course of treatment are presented.
-        More detailed statistics can be accessed ",
-            tags$a(
-              href = "https://digital.nhs.uk/data-and-information/data-collections-and-data-sets/data-sets/improving-access-to-psychological-therapies-data-set/improving-access-to-psychological-therapies-data-set-reports",
-              target = "_blank",
-              "here."
-            )
-          ),
-          tags$p(
-            "Discharged beds indicates the total number of patients discharged from
-        beds, and the percentage this makes up of all beds. More detailed
-        breakdowns can be viewed ",
-            tags$a(
-              href = "https://www.england.nhs.uk/statistics/statistical-work-areas/discharge-delays-acute-data/",
-              target = "_blank",
-              "here."
-            )
-          ),
-          tags$p(
-            "Beds not meeting criteria to reside shows the number of patients who
-        are no longer eligible to occupy a bed, and the percentage of these of
-        all beds. This indicator is often a good proxy for where social care is
-        low. For a more detailed breakdown, see",
-            tags$a(
-              href = "https://www.england.nhs.uk/statistics/statistical-work-areas/discharge-delays-acute-data/",
-              target = "_blank",
-              "here."
-            )
-          ),
-          tags$p(
-            "In addition to the bed availability indicator presented above, our team
-         has also produced a NHS England winter situation report explorer, with
-         detailed breakdowns by type of bed, which can be seen ",
-            tags$a(
-              href = "https://britishredcross.shinyapps.io/sitrep-explorer/",
-              target = "_blank",
-              "here."
-            )
-          )
+          list_indicators[list_indicators$id == "second_care_intro_eng",]$tag,
+          list_indicators[list_indicators$id == "iapt",]$tag,
+          list_indicators[list_indicators$id == "discharged_eng",]$tag,
+          list_indicators[list_indicators$id == "crit_reside_eng",]$tag,
         )
 
         # ---- england_ltla_demographics ----
--- data-raw/list-indicators.R
+++ data-raw/list-indicators.R
@@ -7,10 +7,10 @@ list_indicators <-
   tribble(
     
     # ----Column Names----
-    ~id, ~type , ~indicator, ~tag,
+    ~id, ~type , ~indicator, ~geography, ~tag,
     
     # ----Summary metrics----
-    "summary_intro", "summary_metrics", "intro",  tags$p(
+    "summary_intro", "summary_metrics", "intro", "all", tags$p(
       "These indicators summarise a selection of health metrics into a single
         score. They can be useful for comparing the overall health of different
         areas and are a good place to start. But, they should not be used in
@@ -18,7 +18,7 @@ list_indicators <-
         example, an area may score poorly in a summary metric, yet still excel
         in certain aspects of health."
     ),
-    "health_index", "summary_metrics", "health_index", tags$p(
+    "health_index", "summary_metrics", "health_index", "England", tags$p(
       "The ONS Health Index provides an indication of health outcomes, risk
         factors, and the wider determinants of health. A detailed breakdown
         of the index can be viewed ",
@@ -28,7 +28,7 @@ list_indicators <-
         "here."
       )
     ),
-    "lba", "summary_metrics", "left_behind_areas", tags$p(
+    "lba", "summary_metrics", "left_behind_areas", "England", tags$p(
       "Left-behind areas are places high in deprivation and socio-economic
         challenges, and low in social infrastructure and investment to meet those
         challenges. Research has shown they are associated with high health
@@ -45,7 +45,7 @@ list_indicators <-
         "here."
       )
     ),
-    "imd_england", "summary_metrics", "IMD", tags$p(
+    "imd_england", "summary_metrics", "IMD", "England", tags$p(
       "The Indices of Multiple Deprivation (IMD) include a measure of health
         that measures the risk of premature death and the impairment of quality
         of life through poor physical or mental health. More information can be
@@ -55,7 +55,68 @@ list_indicators <-
         target = "_blank",
         "here."
       )
+    ),
+    
+    # ----Secondary Care----
+    "second_care_intro_eng", "secondary_care", "intro", "England", tags$p(
+      "Secondary care indicators report on the direct performance of the national
+         health service. Most secondary care statistics are reported only at
+         the Trust level. To be able to view these statistics at the local authority
+         level, we have aggregated them using catchment population data.
+         For more information on how we have done this, see ",
+      tags$a(
+        href = "https://britishredcrosssociety.github.io/resilience-index-book/technical.html#health-capacity---england",
+        target = "_blank",
+        "here."
+      )
+    ),
+    "iapt", "secondary_care", "IAPT", "England", tags$p(
+      "The Improving Access to Pyschological Therapies (IAPT) programme offers
+        talking therapies for mental health problems. To address both access to
+        mental health services and the success rate of their interventions, the
+        percentage of referrals that were able to access a service within 18
+        weeks and also finished the first course of treatment are presented.
+        More detailed statistics can be accessed ",
+      tags$a(
+        href = "https://digital.nhs.uk/data-and-information/data-collections-and-data-sets/data-sets/improving-access-to-psychological-therapies-data-set/improving-access-to-psychological-therapies-data-set-reports",
+        target = "_blank",
+        "here."
+      )
+    ),
+    "discharged_eng", "secondary_care", "Discharged beds", "England", tags$p(
+      "Discharged beds indicates the total number of patients discharged from
+        beds, and the percentage this makes up of all beds. More detailed
+        breakdowns can be viewed ",
+      tags$a(
+        href = "https://www.england.nhs.uk/statistics/statistical-work-areas/discharge-delays-acute-data/",
+        target = "_blank",
+        "here."
+      )
+    ),
+    "crit_reside_eng", "secondary_care", "Beds not meeting criteria to reside", "England", tagList(
+      tags$p(
+      "Beds not meeting criteria to reside shows the number of patients who
+        are no longer eligible to occupy a bed, and the percentage of these of
+        all beds. This indicator is often a good proxy for where social care is
+        low. For a more detailed breakdown, see",
+      tags$a(
+        href = "https://www.england.nhs.uk/statistics/statistical-work-areas/discharge-delays-acute-data/",
+        target = "_blank",
+        "here."
+      )
+    ),
+    tags$p(
+      "In addition to the bed availability indicator presented above, our team
+         has also produced a NHS England winter situation report explorer, with
+         detailed breakdowns by type of bed, which can be seen ",
+      tags$a(
+        href = "https://britishredcross.shinyapps.io/sitrep-explorer/",
+        target = "_blank",
+        "here."
+        )
+      )
     )
+    
     
   )
 
