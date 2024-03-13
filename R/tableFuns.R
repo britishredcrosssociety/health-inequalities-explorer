f901464eeb66aae817476a13a096e6610d145db5
Matthew Gwynfryn Thomas <matthewgthomas@gmail.com>
2024-03-13 17:53:24 +0000

    Highlight lowest values in each selected area

--- R/tableFuns.R
+++ R/tableFuns.R
@@ -14,8 +14,8 @@ table_prep <- function(data, selected_areas) {
   
   data[, c("National Mean", selected_areas)] <- round(data[, c("National Mean", selected_areas)], 0)
   
-  # data <- data |> 
-  #   tidyr::pivot_longer(cols = -variable) |> 
+  # data <- data |>
+  #   tidyr::pivot_longer(cols = -variable) |>
   #   tidyr::pivot_wider(names_from = "variable", values_from = "value")
   
   # data$area_name <- string_wrap(data$area_name)
@@ -215,8 +215,27 @@ table_prep <- function(data, selected_areas) {
 
 # ---- Show table for selected areas (or just national mean if no areas have been selected) ----
 table_selected <- function(data, selected_areas) {
-  formattable(
-    data,
-    list()
-  )
+  if (ncol(data) >= 3) {
+    # User has selected at least one area; highlight the minimum values for each sub-domain in the selected areas
+    formattable(
+      data,
+      
+      # For each of the areas selected by the user (columns 3 and beyond), use a <span> tag to highlight the minimum values in red
+      lapply(data[, 3:ncol(data)], function(x) {
+        formatter("span",
+                  style = x ~ formattable::style(
+                    display = "block",
+                    padding = "0 4px", 
+                    `border-radius` = "4px",
+                    color = ifelse(x == min(x, na.rm = TRUE), "white", NA),
+                    `background-color` = ifelse(x == min(x, na.rm = TRUE), "#ee2a24", NA)
+                  ))
+      })
+    )
+  } else {
+    # User hasn't selected any areas; just show the sub-domains and the national mean
+    formattable(
+      data
+    )
+  }
 }
