250616cd217a70c22f88c3bfca4da35df40ba98b
Matthew Gwynfryn Thomas <matthewgthomas@gmail.com>
2024-03-20 11:51:39 +0000

    Use polygons not points

--- data-raw/brc-regions.R
+++ data-raw/brc-regions.R
@@ -50,40 +50,75 @@ usethis::use_data(brc_london_shp, overwrite = TRUE)
 # ---- Health Boards within BRC regions ----
 # Use the centroids of Health Boards to subset by BRC region
 points_icb22 <- 
-  england_icb_shp |>
+  boundaries_icb22 |>
   st_centroid()
 
-brc_north_icb_shp <- 
+# Get list of ICB codes within this region's Local Authorities
+brc_north_icb_codes <- 
   st_join(points_icb22, brc_north_shp) |> 
-  filter(!is.na(area_code.y)) |> 
-  select(area_code = area_code.x, area_name = area_name.x)
+  filter(!is.na(area_code)) |> 
+  st_drop_geometry() |> 
+  select(icb22_code)
+
+# Subset the ICB polygons in this region
+brc_north_icb_shp <- 
+  england_icb_shp |> 
+  filter(area_code %in% brc_north_icb_codes$icb22_code)
 
 usethis::use_data(brc_north_icb_shp, overwrite = TRUE)
 
-brc_london_icb_shp <- 
+# Get list of ICB codes within this region's Local Authorities
+brc_london_icb_codes <- 
   st_join(points_icb22, brc_london_shp) |> 
-  filter(!is.na(area_code.y)) |> 
-  select(area_code = area_code.x, area_name = area_name.x)
+  filter(!is.na(area_code)) |> 
+  st_drop_geometry() |> 
+  select(icb22_code)
+
+# Subset the ICB polygons in this region
+brc_london_icb_shp <- 
+  england_icb_shp |> 
+  filter(area_code %in% brc_london_icb_codes$icb22_code)
 
 usethis::use_data(brc_london_icb_shp, overwrite = TRUE)
 
-brc_central_icb_shp <- 
+# Get list of ICB codes within this region's Local Authorities
+brc_central_icb_codes <- 
   st_join(points_icb22, brc_central_shp) |> 
-  filter(!is.na(area_code.y)) |> 
-  select(area_code = area_code.x, area_name = area_name.x)
+  filter(!is.na(area_code)) |> 
+  st_drop_geometry() |> 
+  select(icb22_code)
+
+# Subset the ICB polygons in this region
+brc_central_icb_shp <- 
+  england_icb_shp |> 
+  filter(area_code %in% brc_central_icb_codes$icb22_code)
 
 usethis::use_data(brc_central_icb_shp, overwrite = TRUE)
 
-brc_south_icb_shp <- 
+# Get list of ICB codes within this region's Local Authorities
+brc_south_icb_codes <- 
   st_join(points_icb22, brc_south_shp) |> 
-  filter(!is.na(area_code.y)) |> 
-  select(area_code = area_code.x, area_name = area_name.x)
+  filter(!is.na(area_code)) |> 
+  st_drop_geometry() |> 
+  select(icb22_code)
+
+# Subset the ICB polygons in this region
+brc_south_icb_shp <- 
+  england_icb_shp |> 
+  filter(area_code %in% brc_south_icb_codes$icb22_code)
 
 usethis::use_data(brc_south_icb_shp, overwrite = TRUE)
 
-brc_southeast_icb_shp <- 
+# Get list of ICB codes within this region's Local Authorities
+brc_southeast_icb_codes <- 
   st_join(points_icb22, brc_southeast_shp) |> 
-  filter(!is.na(area_code.y)) |> 
-  select(area_code = area_code.x, area_name = area_name.x)
+  filter(!is.na(area_code)) |> 
+  st_drop_geometry() |> 
+  select(icb22_code)
+
+# Subset the ICB polygons in this region
+brc_southeast_icb_shp <- 
+  england_icb_shp |> 
+  filter(area_code %in% brc_southeast_icb_codes$icb22_code)
 
 usethis::use_data(brc_southeast_icb_shp, overwrite = TRUE)
