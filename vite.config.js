c9221525bdbf6f807116edc0afb574a98ad6337f
Matthew Gwynfryn Thomas <matthewgthomas@gmail.com>
2024-12-30 12:03:39 +0000

    Add data and charting packages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import dsv from '@rollup/plugin-dsv';
 import path from "path";
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), dsv()],
 	resolve: {
 		alias: {
 			"$actions": path.resolve("./src/actions"),
