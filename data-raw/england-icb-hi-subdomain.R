927d0d001df600c5ad3358d0d2f04deb7df1e966
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2025-01-14 12:02:51 +0100

    Export ICS HI subdomains

--- data-raw/england-icb-hi-subdomain.R
+++ data-raw/england-icb-hi-subdomain.R
@@ -10,18 +10,70 @@ icb <- boundaries_icb22 |>
   mutate(icb22_name = str_remove_all(icb22_name, "^NHS ")) |>
   mutate(icb22_name = str_remove_all(icb22_name, " Integrated Care Board$"))
 
-# ---- Healthy People / Health Outcomes ----
-# england_icb_hi_outcomes_sub <-
-england_health_index_ics_subdomains |>
+outcomes <- c(
+  "difficulties_in_daily_life",
+  "mental_health",
+  "mortality",
+  "personal_well_being",
+  "physical_health_conditions"
+)
+
+risk_factors <- c(
+  "behavioural_risk_factors",
+  "children_and_young_people",
+  "physiological_risk_factors",
+  "protective_measures"
+)
+
+social_determinants <- c(
+  "access_to_green_space",
+  "access_to_services",
+  "crime",
+  "economic_and_working_conditions",
+  "living_conditions"
+)
+
+all_subdomains <- england_health_index_ics_subdomains |>
   filter(year == 2021) |>
   select(-icb22_name, -year) |>
   left_join(icb) |>
   select(-icb22_code) |>
-  rename(area_name = icb22_name) |>
-  relocate(area_name)
+  rename(area_name = icb22_name, variable = sub_domain) |>
+  relocate(area_name) |>
+  pivot_wider(names_from = area_name, values_from = value) |>
+  rowwise() |>
+  mutate(`England Average` = mean(c_across(-variable)), .after = variable) |>
+  ungroup()
 
-# TODO:
-# - filter only relevant sub-domains
-# - calculate england mean
-# - transpose
-# - Do other two subdomains
+# ---- Healthy People / Health Outcomes ----
+england_icb_hi_outcomes_sub <- all_subdomains |>
+  filter(variable %in% outcomes)
+
+# --- Healthy Lives / Preventable Risk Factors ----
+england_icb_hi_risk_factors_sub <- all_subdomains |>
+  filter(variable %in% risk_factors)
+
+# --- Healthy Places / Access to green space ----
+england_icb_hi_social_determinants_sub <- all_subdomains |>
+  filter(variable %in% social_determinants)
+
+# ---- Check distributions ----
+check_distribution <- function(data) {
+  data |>
+    pivot_longer(-c(variable, `England Average`), names_to = "area_name", values_to = "value") |>
+    ggplot(aes(x = value, y = variable)) +
+    geom_density_ridges(scale = 4) +
+    scale_y_discrete(expand = c(0, 0)) + # will generally have to set the `expand` option
+    scale_x_continuous(expand = c(0, 0)) + # for both axes to remove unneeded padding
+    coord_cartesian(clip = "off") + # to avoid clipping of the very top of the top ridgeline
+    theme_ridges()
+}
+
+check_distribution(england_icb_hi_outcomes_sub)
+check_distribution(england_icb_hi_risk_factors_sub)
+check_distribution(england_icb_hi_social_determinants_sub)
+
+# ---- Save datasets ----
+usethis::use_data(england_icb_hi_outcomes_sub, overwrite = TRUE)
+usethis::use_data(england_icb_hi_risk_factors_sub, overwrite = TRUE)
+usethis::use_data(england_icb_hi_social_determinants_sub, overwrite = TRUE)
