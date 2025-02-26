3daed70dd4a68f67afec697466b3aa8f301e513c
Matthew Gwynfryn Thomas <matthewgthomas@gmail.com>
2025-02-26 16:50:11 +0000

    Revert "Use latest LA boundaries"
    This reverts commit adf06627edcdb8e1c7d7ff5569ec9887b16d26e6.

--- data-raw/northern-ireland-ltla-shp.R
+++ data-raw/northern-ireland-ltla-shp.R
@@ -4,9 +4,9 @@ library(geographr)
 library(rmapshaper)
 
 northern_ireland_ltla_shp <-
-  boundaries_ltla24 |>
-  filter(str_detect(ltla24_code, "^N")) |>
+  boundaries_ltla21 |>
+  filter(str_detect(ltla21_code, "^N")) |>
   ms_simplify(keep = 0.5) |>
-  select(area_name = ltla24_name, area_code = ltla24_code)
+  select(area_name = ltla21_name, area_code = ltla21_code)
 
 usethis::use_data(northern_ireland_ltla_shp, overwrite = TRUE)
--- data-raw/wales-ltla-shp.R
+++ data-raw/wales-ltla-shp.R
@@ -4,9 +4,9 @@ library(geographr)
 library(rmapshaper)
 
 wales_ltla_shp <-
-  boundaries_ltla24 |>
-  filter(str_detect(ltla24_code, "^W")) |>
+  boundaries_ltla21 |>
+  filter(str_detect(ltla21_code, "^W")) |>
   ms_simplify(keep = 0.5) |>
-  select(area_name = ltla24_name, area_code = ltla24_code)
+  select(area_name = ltla21_name, area_code = ltla21_code)
 
 usethis::use_data(wales_ltla_shp, overwrite = TRUE)
